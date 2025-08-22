8d666ae328dc7ef5bb69ed8e422ff2ee874c83df
SarveshShahane <shahanesarvesh45@gmail.com>
2025-08-22 18:51:42 +0530

    Deployment updates

--- server/src/server.js
+++ server/src/server.js
@@ -2,19 +2,12 @@ import { app } from "./app.js";
 import connectDB from "./config/db.config.js";
 import { startReminderScheduler } from "./utils/cronJobs.js";
 
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, "../client/dist")));
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "client-dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client-dist", "index.html"));
 });
-
-
 const startServer = async () => {
   await connectDB().then(() => {
     const PORT = process.env.PORT || 5000;
